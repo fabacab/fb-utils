49812ab6b63cee3bcec5452af9644b6106f12ce6
Meitar Moscovitz <meitarm@gmail.com>
2016-05-14 01:27:28 -0600

    Use explicit waits instead of implicit ones, use JS to scroll.

--- who-likes-us.py
+++ who-likes-us.py
@@ -6,7 +6,10 @@
 import sys, os, time, argparse, configparser
 from selenium import webdriver
 from selenium.webdriver.firefox.webdriver import FirefoxProfile
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def parse_arguments():
     """Gathers command-line options.
@@ -119,7 +122,6 @@ def main():
 
     # Drive!
     driver = webdriver.Firefox(profile)
-    driver.implicitly_wait(10) # seconds
 
     for url in urls:
         # Load the page.
@@ -127,14 +129,14 @@ def main():
         # Click the "See More" link until it's gone.
         while True:
             try:
-                el = driver.find_element_by_xpath(xpaths.get('see_more_link'))
-                # implicitly waiting 10 seconds for the element to become
-                # available again. Then, we click it.
-                el.click()
-            except StaleElementReferenceException:
-                # this will be raised when the element we try to `click()`
-                # on no longer exists, because Facebook has removed it.
-                # This means we've reached the end of the "See More" list.
+                wait = WebDriverWait(driver, 10)
+                el = wait.until(
+                    EC.element_to_be_clickable((By.XPATH, xpaths.get('see_more_link')))
+                    )
+                driver.execute_script('arguments[0].scrollIntoView()', el)
+            except TimeoutException:
+                # This will be raised when Facebook removes the element.
+                # It means we've reached the end of the "See More" list.
                 break
 
         # Now the whole list of people who like our page is loaded.
